41c5345fcbb99ea09a6255bc3b7989c2745071fd
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-02-16 18:59:50 +0530

    modiefiers

---
+++ Basic/modiefiers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main (){
+    //int var1 = INT_MIN;
+    //int var2 = INT_MAX;
+
+    unsigned int var1 = 0;
+    unsigned int var2 = UINT_MAX;
+
+    //printf("range of signed INT is: %d to %d", var1, var2);
+    printf("range of unsigned INT is: %u to %u", var1, var2);
+    return 0;
+
+}
