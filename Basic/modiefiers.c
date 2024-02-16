f325716e0611922f01970637c97b29b2da989a6d
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-02-16 20:07:35 +0530

    commit

--- Basic/modiefiers.c
+++ Basic/modiefiers.c
@@ -5,11 +5,15 @@ int main (){
     //int var1 = INT_MIN;
     //int var2 = INT_MAX;
 
-    unsigned int var1 = 0;
-    unsigned int var2 = UINT_MAX;
+    //unsigned int var1 = 0;
+    //unsigned int var2 = UINT_MAX;
+
+    short int var1 = SHRT_MIN;
+    short int var2 = SHRT_MAX;
 
     //printf("range of signed INT is: %d to %d", var1, var2);
-    printf("range of unsigned INT is: %u to %u", var1, var2);
+    //printf("range of unsigned INT is: %u to %u", var1, var2);
+    printf("range of short signed int : %d to %d", var1, var2);
     return 0;
 
 }
