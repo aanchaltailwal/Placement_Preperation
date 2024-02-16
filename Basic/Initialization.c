4fa617682810d719087b38079db31ac9c7ed1041
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-02-16 15:56:36 +0530

    initilization

--- Basic/Initialization.c
+++ Basic/Initialization.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    int var = 3;
-    var = 4;  // no need to assign int again as alsreday assigned in memmory
-    printf("%d", var);
+    //int var = 3;
+    //var = 4;  // no need to assign int again as alsreday assigned in memmory
+    //printf("%d", var);
+
+    int var1 = 3;
+    int var2;
+    var2 = 5;
+    printf("%d", var2);
 
     return 0;
 }
