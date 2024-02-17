2d6ebe9154e9f0e20597d5f260e9d6b5bb77ac93
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-02-17 23:53:15 +0530

    scanf

---
+++ Basic/Inputfunc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    printf("Enter the first no. :\n");
+    scanf("%d", &a);
+    printf("Enter the second no. :\n");
+    scanf("%d", &b);
+    printf("%d + %d = %d", a , b , a+b);
+    return 0;
+
+}
