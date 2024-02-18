c90df2a8bf20d02183e2551846c8d063eb986e0f
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-02-18 19:19:26 +0530

    relational

--- Operators/Quest2.c
+++ Operators/Quest2.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main(){
     int a = 4, b = 3;
-    printf("%d", a+ ++b);
+    printf("%d", a+  ++b);
+   // printf("%d", a+++++b); //lvalue req as inc operand
+   
     return 0;
 }
---
+++ Operators/Relatinalop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a = 300, b = 2090;
+    if (b >= a){
+    printf("Bingo");
+}
+else{
+    printf("OOps");
+}
+
+}
