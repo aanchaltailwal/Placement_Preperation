6969466c81584d2e89333b1e6afbceb74c1f357e
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-03-22 17:33:05 +0530

    leap year

--- Special_Programs/FibinacciSeries.c
+++ Special_Programs/FibinacciSeries.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main(){
-    int i, j, rows, n=1;
-    printf("Enter the no. of rows: ");
-    scanf("%d", &rows);
+    int a, b, result, n, i;
+    printf("enter the numebr of terms: ");
+    scanf("%d", &n);
 
-    for(i=1; i<=rows; i++){
-        for(j=1; j<=i; j++){
-            printf("%d", n);
-            n++;
-        }
-        printf("\n");   
+    a=0;
+    b=1;
+
+    for(i=1; i <= n; i++){
+        printf("%d\n", a);
+        result = a + b;
+        a = b;
+        b = result;
     }
-    return 0;
 }
