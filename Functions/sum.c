bb5052e1812fe4e63880d47da5b0a3ec3856b308
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-04-01 12:24:19 +0530

    sum.c

---
+++ Functions/sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int add (int, int);
+int main(){
+    int m=20, n=30, sum;
+    sum = add(m,n);
+    printf("sum is %d", sum);
+}
+int add (int a , int b){
+    return(a + b);
+}
