43ba54f67f8ec8e91ab465bea73232fa423935cd
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-06-07 21:23:47 +0530

    question 2

---
+++ Placement Preperation/Question2.c
@@ -0,0 +1,35 @@
+/*Sumita and equal array
+Problem Statement:
+Sumita is playing with a Array A of size N. she wants to make all the values of A to be equal. she can multiply any value any number of times by X, Y and Z. Your task is to tell her whether she can do it or not. Print "She can" if she can do it else print "She can't" without ""
+
+Input:
+First line of the input will contain T (No. of test cases).
+For each test case, first line will contain four space separated integers denoting N, X, Y and Z.
+Then next line will contain N space separated integers of A
+
+Output:
+For every test case, print the required answer in a new line.
+
+Constraints:
+1 ≤ T ≤ 5
+2 ≤ N ≤ 10^5
+X, Y, Z ∈ {2, 3, 5, 7}
+1 ≤ Ai ≤ 10^9
+
+Time Limit:
+1 sec
+
+Example 1:
+Input
+2
+2 2 2 2
+2 4
+3 2 3 2
+2 6 7
+
+Output
+She can
+She can't
+
+Sample I/O Explanation:*/
+
